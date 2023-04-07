82e9c9ff8ebaae0ed87ebf3ef173d85db4ebc0b8
Julio Neto <nettojulio@hotmail.com>
2023-04-07 01:42:16 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Estrutura Sequencial
+
+## Projeto consiste em aprimorar e aprofundar conhecimentos com a linguagem Java, utilizando uma lista de atividades propostas pela comunidade Python Brasil
+
+![Image](https://wiki.python.org.br/pybr/img/pythonbrasil_logo.png)
+
+[Lista de exercícios](https://wiki.python.org.br/EstruturaSequencial)
+
+---
+
+## Exercício 006
+
+- [x] Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+
+### Aluno: Julio Neto
---
+++ src/main/java/br/com/julioneto/App.java
@@ -0,0 +1,44 @@
+package br.com.julioneto;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class App {
+    private static Double valor = null;
+    private static Double area = null;
+
+    public static void main(String[] args) {
+        entrada();
+        processamento();
+        saida();
+    }
+
+    private static void entrada() {
+        Scanner teclado = new Scanner(System.in);
+        teclado.useLocale(Locale.US);
+
+        do {
+            System.out.print("Digite o raio da circunferência (em cm): ");
+            String valorDigitado = teclado.nextLine().replace(",", ".");
+            validacoesDeEntrada(valorDigitado);
+        } while (valor == null);
+
+        teclado.close();
+    }
+
+    private static void validacoesDeEntrada(String valorDigitado) {
+        try {
+            valor = Double.parseDouble(valorDigitado);
+        } catch (Exception e) {
+            System.out.println("Erro ao digitar o raio, tente novamente!");
+        }
+    }
+
+    private static void processamento() {
+        area = Math.pow(valor, 2) * Math.PI;
+    }
+
+    private static void saida() {
+        System.out.printf("A área dacircunferência é %.2f cm", area);
+    }
+}
